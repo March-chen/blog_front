7567810e7b5e67b8e035262e1e0f811a2ae2805a
ccc <xautchenping@gmail.com>
2022-03-06 17:58:33 +0800

    del useless code

--- src/buttom/Buttom.vue
+++ src/buttom/Buttom.vue
@@ -1,9 +1,9 @@
 <template>
-    <div style="width:300px;margin:0 auto; padding:20px 0;">
+    <footer>
       <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010802038336"
           style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="@/assets/beian.png" style="float:left;"/>
-        <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">京公网安备 11010802038336号</p></a>
-		</div>
+      <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;text-align: center">京公网安备 11010802038336号</p></a>
+    </footer>
 </template>
 
 <script>
@@ -13,9 +13,10 @@ export default {
 </script>
 
 <style scoped>
-footer{
-width: 100%;
-position: absolute;
-bottom: 0
+  footer{
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  left : 100;
 }
 </style>
